98f7c2adb03c31897bb10a840c8e5793cad1dbd9
C-hill10 <osuconnor@live.com>
2026-02-13 21:16:25 -0600

    hitler is dead

--- maze_solver.py
+++ maze_solver.py
@@ -7,59 +7,56 @@ class Maze_solver:
         self.Maze=Maze
         self.solution=np.zeros(shape=(self.Maze.size*2+1,self.Maze.size*2+1),dtype=int)
     def solve_maze(self):
-        print(self.Maze.maze)
         visited=np.zeros(shape=(self.Maze.size*2+1,self.Maze.size*2+1),dtype=int)
-        checkpointlist=[(1,1)] #holds the locations of branching paths to return back to
+        checkpointlist=[] #holds the locations of branching paths to return back to
         maze_state_list=[visited] #intending this to hold the state of the maze at each checkpoint, doesnt work
         dead_ends=[]
-        current_trial=()
+        current_trial=[]
         position=(1,1) #starting position
         done=False
-        counter=0
+        justjumped=False
         while not done:
-            counter+=1
             x,y=position
             visited[x][y]=1
             path_counter=0
             neighbor_coords = (x+1, y), (x, y+1), (x-1, y), (x, y-1)
             for neighbor in neighbor_coords:
                 neighborx,neighbory=neighbor
-                if self.Maze.maze[neighborx][neighbory]==1 and visited[neighborx][neighbory]==0:
+                if self.Maze.maze[neighborx][neighbory]==1 and visited[neighborx][neighbory]==0 and neighbor not in dead_ends:
                     path_counter+=1
                 if neighbor==self.Maze.goal_position:
                     self.solution=visited
                     done=True
-                    continue
-            if path_counter==0:
-                #This is a dead end, would want to backtrack to the last crossroads
-                visited[x][y]=0
-                dead_ends.append(current_trial)
-                position = checkpointlist.pop(-1)
-                print(visited)
-                visited=maze_state_list.pop(-1)
-                print("Checking previous states")
-                for path in reversed(maze_state_list):
-                    print(path)
-                    print("\n\n\n\n")
-                print(visited)
+                    break
+            if not done:
+                if path_counter==0:
+                    #This is a dead end, would want to backtrack to the last crossroads
+                    dead_ends.append(current_trial.pop())
+                    position = checkpointlist.pop()
+                    visited=maze_state_list.pop()
+                    justjumped=True
 
-                continue
-            if path_counter==1:
-                #go to the next open spot
-                for neighbor in neighbor_coords:
-                    neighborx,neighbory=neighbor
-                    if visited[neighborx][neighbory]==0 and self.Maze.maze[neighborx][neighbory]==1 and neighbor not in dead_ends:
-                        position=neighbor
-                        continue
-            if path_counter==2:
-                #at a crossroads, we want to save our spot so we can look again later
-                checkpointlist.append((x,y))
-                maze_state_list.append(np.zeros(shape=(self.Maze.size*2+1,self.Maze.size*2+1),dtype=int)+visited)
-                for neighbor in neighbor_coords:
-                    neighborx,neighbory=neighbor
-                    if visited[neighborx][neighbory]==0 and self.Maze.maze[neighborx][neighbory]==1 and neighbor not in dead_ends:
-                        position=neighbor
-                        current_trial=neighbor
+                elif path_counter==1:
+                    #go to the next open spot
+                    for neighbor in neighbor_coords:
+                        neighborx,neighbory=neighbor
+                        if visited[neighborx][neighbory]==0 and self.Maze.maze[neighborx][neighbory]==1 and neighbor not in dead_ends:
+                            position=neighbor
+                            if justjumped:
+                                current_trial.append(neighbor)
+                                justjumped=False
+                            break
+                elif path_counter==2:
+                    #at a crossroads, we want to save our spot so we can look again later
+                    checkpointlist.append((x,y))
+                    maze_state_list.append(np.zeros(shape=(self.Maze.size*2+1,self.Maze.size*2+1),dtype=int)+visited)
+                    for neighbor in neighbor_coords:
+                        neighborx,neighbory=neighbor
+                        if visited[neighborx][neighbory]==0 and self.Maze.maze[neighborx][neighbory]==1 and neighbor not in dead_ends:
+                            position=neighbor
+                            current_trial.append(neighbor)
+                            break
+
 if __name__ == "__main__":
     place=Maze(5)
     solver=Maze_solver(place)
