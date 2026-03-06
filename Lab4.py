f40b60203d41cca1a39b125dfbd6f0c52b85ccce
C-hill10 <osuconnor@live.com>
2026-03-06 13:26:01 -0600

    worked in class, figuring out plots

--- Lab4.py
+++ Lab4.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 def bisect(func,a,b,tol):
     while abs(a-b)>tol:
-        xm=a+b
+        xm=(a+b)/2
         result=func(xm)
         if result*func(a)>0:
             a=xm
@@ -13,6 +13,7 @@ def bisect(func,a,b,tol):
 def regula_falsi(func,a,b,tol):
     while abs(a-b)>tol:
         xr=(a*func(b)-b*func(a))/(func(b)-func(a))
+        result=func(xr)
         if result*func(a)>0:
             a=xr
         else:
@@ -30,10 +31,37 @@ def nr(func,x,tol,derivative):
         yield x
 def secant(func,x,x1,tol):
     while abs(x-x1)>tol:
-        x2=x1-func(x1)*(x1-x0)/(func(x1)-func(x0))
+        x2=x1-func(x1)*(x1-x)/(func(x1)-func(x))
         x=x1
         x1=x2
         yield x2    
 
 def error(solution,func,method,*args,**kwargs):
-    x=method(func,)
+    # kwargs are plot_error and plot_convergence (bools)
+    print(f'args 0: {args[0]},args 1: {args[1]},args 2: {args[2]} ')
+    x=method(func,args[0],args[1],args[2])
+    guesses=list(x)
+    if bool(kwargs["plot_error"]):
+        error=[]
+        for answer in guesses:
+            error.append(abs(answer-solution))
+        fig=plt.figure()
+        ax=fig.add_subplot(1,1,1)
+        line,=ax.plot(error,color='blue')
+        ax.set_yscale('log')
+        plt.grid()
+        plt.show(block=False)
+    if bool(kwargs['plot_convergence']):
+        nextlist=guesses[1:]
+        guesses=guesses[:-1]
+        fig=plt.figure()
+        ax=fig.add_subplot(1,1,1)
+        line,=ax.plot(guesses,nextlist,color='red')
+        ax.set_yscale('log')
+        plt.grid()
+        plt.show(block=True)
+    print(list(x))
+
+if __name__=="__main__":
+    func=lambda x: np.sin(x)
+    error(0,func,bisect,-2,1,1e-5,plot_error=True,plot_convergence=True)
