5e8dca20341facb98dc9b4f50c9021173aef4cbb
C-hill10 <osuconnor@live.com>
2026-03-13 14:30:28 -0500

    that was scary

--- Hw22.py
+++ Hw22.py
@@ -20,10 +20,27 @@ def make_T_matrix(num_rows):
             t_matrix[i][i+1]=1
         t_matrix[i][i-1]=1
     return t_matrix
-def draw_solution(solution_matrix):
-    plt.imshow(solution_matrix,cmap="inferno")
-    plt.colorbar()
-    plt.show()
+
+def make_b_vector(num_points):
+    b_vector=np.zeros((num_points**2,1))
+    total_points=num_points+2 #account for grid spacing counting as points
+    point_values=np.linspace(-1,1,total_points)
+    print(point_values)
+    
+    f= lambda x,y: 25-x**2+y**2
+    for y in range(0,num_points):
+        for x in range(0,num_points):
+            if y==0:
+                b_vector[y*num_points+x][0]+=f(point_values[x+1],-1)
+            if y==num_points-1:
+                b_vector[y*num_points+x][0]+=f(point_values[x+1],1)
+            if x==0:
+                b_vector[y*num_points+x][0]+=f(-1,point_values[y+1])
+            if x==num_points-1:
+                b_vector[y*num_points+x][0]+=f(1,point_values[y+1])
+    print(f"spacing is {point_values[1]}")
+    b_vector=b_vector*(-1/(point_values[1]**2))
+    return b_vector
 
 
 def make_A_matrix(num_rows_a,num_rows_t):
@@ -36,7 +53,6 @@ def make_A_matrix(num_rows_a,num_rows_t):
         if i<=num_rows_a-2:
             a_matrix[i][i+1]=np.identity(num_rows_a)
         a_matrix[i][i-1]=np.identity(num_rows_a)
-    print(a_matrix)
     return a_matrix
 
 def cholesky_decomp(Symmetric_matrix):
@@ -56,10 +72,17 @@ def cholesky_decomp(Symmetric_matrix):
             Upper_triangle[row][j]=(Symmetric_matrix[row][j]-intermediate_sum)/Upper_triangle[row][row]
     print(Upper_triangle)
     return Upper_triangle
-
+def make_big(matrix,size_square):
+    matrix=np.block([*matrix])
+    return np.reshape(matrix,(size_square**2,size_square**2))
 if __name__=="__main__":
-    #mymatrix=make_A_matrix(3,3)
-    testmatrix=np.array([[6,15,55],[15,55,225],[55,225,979]])
-    upper_triangle=cholesky_decomp(testmatrix)
-    draw_solution(testmatrix)
+    mymatrix=make_A_matrix(10,10)
+    mymatrix=make_big(mymatrix)
+    #testmatrix=np.array([[6,15,55],[15,55,225],[55,225,979]])
+    #cholesky_decomp(testmatrix)
+    b_vector=make_b_vector(10)
+    mymatrix=cholesky_decomp(mymatrix)
+    d_vector=forward_substitution(mymatrix,b_vector)
+    print(b_vector)
+
 
--- Lab4.py
+++ Lab4.py
@@ -58,7 +58,7 @@ def secant(func,x,x1,tol):
     counter=0
     while abs(x-x1)>tol:
         if counter>=limit:
-                raise ConvergenceError("exceeded global iteration limit")
+            raise ConvergenceError("exceeded global iteration limit")
         yield x
         x2=x1-func(x1)*(x1-x)/(func(x1)-func(x))
         x=x1
@@ -70,7 +70,7 @@ def golden_section(func,a,b,epsilon):
     phi = (math.sqrt(5)+1)/2
     while abs(a-b)>epsilon:
         if counter>=limit:
-                raise ConvergenceError("exceeded global iteration limit")
+            raise ConvergenceError("exceeded global iteration limit")
         x1=b-(b-a)/phi
         x2=a+(b-a)/phi
         if func(x1)<func(x2):
@@ -111,6 +111,7 @@ def error(solution,func,method,*args,**kwargs):
     for entry in range(0,len(guesses)-1):
         #Entry in book mentioned formula being related to error and exponential
         #E_n+1=K*(E_n)^p, using log to solve for p with plot fitting
+        #For this part, i got help from friends to troubleshoot it and make it work
         errorlogx.append(math.log(error[entry]))
         errorlogy.append(math.log(error[entry+1]))
 
@@ -143,25 +144,25 @@ if __name__=="__main__":
 
     #Regula falsi test with same parameters as bisection
     func=lambda x: np.sin(x)
-    deg = error(0,func,regula_falsi,-2,1,1e-5,plot_error=False,plot_convergence=False)
+    deg = error(0,func,regula_falsi,-2,1,1e-5,plot_error=1,plot_convergence=1)
     print(f"convergence degree of Regula falsi is {deg[0]}")
 
     #newton-raphson testing
     func= lambda x:-0.87*x**2+1.65*x+8.25
     derivative= lambda x: -1.74*x+1.65
-    deg=error(4.17038,func,nr,3,1e-8,derivative,plot_error=0,plot_convergence=0)
+    deg=error(4.17038,func,nr,3,1e-8,derivative,plot_error=1,plot_convergence=0)
     print(f"convergence degree of NR is {deg[0]}")
     #secant method test to see if i have it right for order of convergence
     func= lambda x:-0.87*x**2+1.65*x+8.25
     derivative= lambda x: -1.74*x+1.65
-    deg=error(4.17038,func,secant,3.5,4.5,1e-8,plot_error=0,plot_convergence=0)
+    deg=error(4.17038,func,secant,3.5,4.5,1e-8,plot_error=1,plot_convergence=1)
     print(f"convergence degree of Secant is {deg[0]}")
     #Fixed point test, the fixed point is 0
     func=lambda x: x**2
-    deg= error(0,func,fixed_point,0.5,1e-3,plot_error=0,plot_convergence=False)
+    deg= error(0,func,fixed_point,0.5,1e-3,plot_error=1,plot_convergence=1)
     print(f"convergence degree of fixed point is {deg[0]}")
 
     #optimization test 
     func= lambda x: (-.3*x**4)+(1.8*x**3)-(1.2*x**2)+2*x
-    deg= error(4.1122,func,golden_section,3,5,1e-5,plot_error=0,plot_convergence=False)
+    deg= error(4.1122,func,golden_section,3,5,1e-5,plot_error=1,plot_convergence=1)
     print(f"convergence degree of golden section is {deg[0]}")
