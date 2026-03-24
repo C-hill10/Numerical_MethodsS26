18405424c3f1559ea50deb640b3bb96c47ad7c98
C-hill10 <osuconnor@live.com>
2026-03-23 19:21:21 -0500

    i thought earlier

--- Lab5/CooleyTukey.py
+++ Lab5/CooleyTukey.py
@@ -2,4 +2,9 @@ import math
 
 
 def FFT(sequence):
-    
+
+    if abs(math.log(len(sequence),2)-math.floor(math.log(len(sequence),2))) > 1e-9:
+        raise ValueError("input sequence does not have length of power of 2")
+
+    def _recursiveFFT(Sequence):
+        return 0
