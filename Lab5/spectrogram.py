0077e3fbcc062b2812770d639ad3a415775bc4d4
C-hill10 <osuconnor@live.com>
2026-04-01 10:39:56 -0500

    why graph no work

--- Lab5/spectrogram.py
+++ Lab5/spectrogram.py
@@ -3,24 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import CooleyTukey
 import sounddevice as sd
-import scipy.interpolate 
-fs=44100
-seconds=3 # 4 seconds does not work, get the edge case data, 1 second does work
+
+#originally 44100
+fs=20000
+seconds=1 # 4 seconds does not work, get the edge case data, 1 second does work
 if __name__ == "__main__":
     # Generate a sample noise signal
-    t = np.arange(0.0, 20.5, 0.02) #changed to 05 instead of 0005
-    s1 = np.sin(2*np.pi*t)
-    s2 = 2*np.sin(2*np.pi*3*t)
-    s2[t <= 10] = s2[12 <= t] = 0
-    noise = 0.01*np.random.random(size=len(t))
-    x = s1 + s2 + noise
-    print(f"x and t shape {x.shape}, {t.shape}")
-    t2=np.arange(0.0, 20.5, 0.01)
-    # x=np.arange(0.0,3,1/44100)
+    # t = np.arange(0.0, 20.5, 0.02) #changed to 05 instead of 0005
+    # s1 = np.sin(2*np.pi*t)
+    # s2 = 2*np.sin(2*np.pi*3*t)
+    # s2[t <= 10] = s2[12 <= t] = 0
+    # noise = 0.01*np.random.random(size=len(t))
+    # x = s1 + s2 + noise
+    # print(f"x and t shape {x.shape}, {t.shape}")
+    # t2=np.arange(0.0, 20.5, 0.01)
+    t1=np.arange(0.0,1,1/fs)
+    t2=np.arange(0.0,1,1/(2*fs))
+    print(f'shake of t1 and t2 are {t1.shape}, {t2.shape}')
     # TODO: This does work, just be careful with time you sample
-    # print(f"starting recording for {seconds} seconds")
-    # x=sd.rec((int(seconds * fs)),samplerate=fs,channels=1)
-    # sd.wait()
+    print(f"starting recording for {seconds} seconds")
+    x=sd.rec((int(seconds * fs)),samplerate=fs,channels=1)
+    x=x.squeeze()
+    sd.wait()
+    print(f"shape of x is {x.shape}")
     # Iterate over the windowed audio and compute power spectrum data
     psds = []
     N = 1024 # samples per chunk of windowed audio
@@ -36,11 +41,11 @@ if __name__ == "__main__":
     # Plot the PSDs as a spectrogram
     # TODO: Add a subplot showing the interpolated audio waveform (with shared x-axis)
     # TODO: Label and correct the x-axis and y-axis values
-    interpolation=scipy.interpolate.CubicSpline(t,x,bc_type="natural")
-    my_spline=CooleyTukey.CubicSpline(t,x) #this worked for small data values, but i got a memory error for trying to hold the whole spline
+    #interpolation=scipy.interpolate.CubicSpline(t,x,bc_type="natural")
+    my_spline=CooleyTukey.CubicSpline(t1,x) #this worked for small data values, but i got a memory error for trying to hold the whole spline
     fig,(ax0,ax1)=plt.subplots(2,1,sharex=True,gridspec_kw={'height_ratios':[1,2]})
     #ax0.plot(t,x)
-    ax0.plot(t2,CooleyTukey.interpolate(my_spline,t2,t,0.02))
+    ax0.plot(t2,CooleyTukey.interpolate(my_spline,t2,t1,1/fs))
     #ax[0].title.set_text("Interpolated waveform")
     ax0.set_ylabel("magnitude")
     ax1.set_xlabel("Time (s)")
